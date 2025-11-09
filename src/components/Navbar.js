9767019935c76cb5bf14c46e8c42e16c734edb2c
nimipam96 <pamnaninimit16@gmail.com>
2025-11-09 23:07:11 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import "../styles/Navbar.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+//     <nav className="navbar">
+//       <div className="logo">Vanshita</div>
+//       <ul className="nav-links">
+//         <li>
+//           <a href="#home">Home</a>
+//         </li>
+//         <li>
+//           <a href="#about">About</a>
+//         </li>
+//         <li>
+//           <a href="#skills">Skills</a>
+//         </li>
+//         <li>
+//           <a href="#projects">Projects</a>
+//         </li>
+//         <li>
+//           <a href="#contact">Contact</a>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+
+export default NavBar;
