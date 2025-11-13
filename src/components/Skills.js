365dabb39f16144d6084ffd133031524e6375116
vanshita <pamnanivanshita@gmail.com>
2025-11-13 22:54:54 +0530

    SkillsPage80% done

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,31 @@
 import "../styles/skills.css";
 
 function Skills() {
-  return;
+  return (
+    <div className="outer-container">
+      <div className="inner-container">
+        <img src="/image/html5.svg" alt="HTML" />
+        <p>HTML</p>
+      </div>
+      <div className="inner-container">
+        <img src="/image/css.svg" alt="CSS" />
+        <p>CSS</p>
+      </div>
+      <div className="inner-container">
+        <img src="/image/bootstrap.svg" alt="BOOTSTRAP" />
+        <p>BOOTSTRAP</p>
+      </div>
+      <div className="inner-container">
+        <img src="/image/javascript.svg" alt="JAVASCRIPT" />
+        <p>JAVASCRIPT</p>
+      </div>
+
+      <div className="inner-container">
+        <img src="/image/react.svg" alt="REACT" />
+        <p>REACT</p>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
