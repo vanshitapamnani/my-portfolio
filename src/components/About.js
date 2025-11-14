fae81a610aafd650a27137eeb6a81d5e9942df0b
vanshita <pamnanivanshita@gmail.com>
2025-11-14 16:20:17 +0530

    contactPageDone

--- src/components/About.js
+++ src/components/About.js
@@ -2,33 +2,39 @@ import "../styles/about.css";
 
 function About({ handleNext }) {
   return (
-    <div className="about">
-      <p className="about-text">
-        I’m Vanshita Pamnani, A{" "}
-        <span className="highlight">Front-End Developer </span> Passionate About
-        Building <span className="highlight">Visually Engaging </span> And
-        <span className="highlight"> Performance-Driven</span> Web Applications.
-        With A Background In{" "}
-        <span className="highlight">Nutrition and Dietetics </span>, I Bring A
-        Unique Mix Of{" "}
-        <span className="highlight">Analytical Problem-Solving </span> and
-        <span className="highlight"> Creative Storytelling</span> To My
-        Development Process. I Focus On writing{" "}
-        <span className="highlight"> Clean</span>,{" "}
-        <span className="highlight">Efficient Code</span> and Crafting
-        Interfaces That Not Only Look Great But Also Offer{" "}
-        <span className="highlight">Seamless User Experiences.</span> Currently,
-        I’m Exploring Modern Frameworks Like{" "}
-        <span className="highlight">React </span>and Working On Projects That
-        Help Me Grow As Both A<span className="highlight"> Developer </span> .
-      </p>
+    <>
+      <div className="about">
+        <p className="about-text">
+          I’m Vanshita Pamnani, A{" "}
+          <span className="highlight">Front-End Developer </span> Passionate
+          About Building <span className="highlight">Visually Engaging </span>{" "}
+          And
+          <span className="highlight"> Performance-Driven</span> Web
+          Applications. With A Background In{" "}
+          <span className="highlight">Nutrition and Dietetics </span>, I Bring A
+          Unique Mix Of{" "}
+          <span className="highlight">Analytical Problem-Solving </span> and
+          <span className="highlight"> Creative Storytelling</span> To My
+          Development Process. I Focus On writing{" "}
+          <span className="highlight"> Clean</span>,{" "}
+          <span className="highlight">Efficient Code</span> and Crafting
+          Interfaces That Not Only Look Great But Also Offer{" "}
+          <span className="highlight">Seamless User Experiences.</span>{" "}
+          Currently, I’m Exploring Modern Frameworks Like{" "}
+          <span className="highlight">React </span>and Working On Projects That
+          Help Me Grow As Both A<span className="highlight"> Developer </span> .
+        </p>
 
-      <p>
-        Front-End Developer in Progress | Clinical Dietitian | Bridging Wellness
-        & Tech
-      </p>
-      <button onClick={handleNext}>Next</button>
-    </div>
+        <p>
+          Front-End Developer in Progress | Clinical Dietitian | Bridging
+          Wellness & Tech
+        </p>
+        <div>
+          {" "}
+          <button onClick={handleNext}>Next</button>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,24 @@
 import "../styles/contact.css";
 
-function Contact() {
-  return;
+function Contact({ handleNext }) {
+  return (
+    <>
+      <div className="contact-outer">
+        <div className="contact-inner">
+          <div className="contact-heading">
+            <h1> Want To Connect ?</h1>
+          </div>
+
+          <p>📩 Email - pamnanivanshita@gmail.com</p>
+          <p>🔗 LinkedIn - https://www.linkedin.com/in/vanshitapamnani/ </p>
+          <p>💻 GitHub - https://github.com/vanshitapamnani</p>
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={handleNext}>Back to Home</button>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
