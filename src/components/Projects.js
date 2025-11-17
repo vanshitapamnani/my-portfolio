b74658d4556e3c1286949cd79305933e9daba214
vanshita <pamnanivanshita@gmail.com>
2025-11-17 23:50:57 +0530

    WorkingOnProjectPage

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,19 @@
 import "../styles/project.css";
 function Projects() {
   return (
-    <div className="project">
-      <h1>Projects I've Done</h1>
-    </div>
+    <>
+      <div className="project">
+        <h1>Projects I've Done</h1>{" "}
+      </div>
+      <div>
+        <p>
+          These projects represent my journey as a front-end developer, where I
+          experiment, learn, and build visually appealing, functional
+          interfaces. Each project has helped me strengthen my skills and
+          understand real-world web development.
+        </p>
+      </div>
+    </>
   );
 }
 
