e39f4ecf84f5c51b6e0ccfe347c5fb0e3e534d0f
vanshita <pamnanivanshita@gmail.com>
2025-11-18 23:07:28 +0530

    flip feature added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,15 +3,29 @@ function Projects() {
   return (
     <>
       <div className="project">
-        <h1>Projects I've Done</h1>{" "}
+        <h1>Projects I've Done</h1>
       </div>
-      <div>
+      <div className="para">
         <p>
           These projects represent my journey as a front-end developer, where I
           experiment, learn, and build visually appealing, functional
           interfaces. Each project has helped me strengthen my skills and
           understand real-world web development.
         </p>
+      </div>
+      <div className="out-container">
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src="image/project/pizzaa.png" alt="pizza menu" />
+            </div>
+
+            <div className="flip-card-back">
+              <h3>React Project</h3>
+              <p>This is a pizza menu webpage.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
